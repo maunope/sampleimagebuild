9817c5183befb7314fb82fa96256b6f7c5c3f918
maunope <mnoseda@google.com>
2025-11-10 22:48:51 +0100

    test

--- petshopdeploy/main.tf
+++ petshopdeploy/main.tf
@@ -405,6 +405,6 @@ resource "google_compute_global_forwarding_rule" "petshop_forwarding_rule" {
 # -----------------------------------------------------------------------------
 
 output "website_ip" {
-  description = "The public IP address of the Pet Shop website."
+  description = "the public IP address of the Pet Shop website."
   value       = google_compute_global_forwarding_rule.petshop_forwarding_rule.ip_address
 }
